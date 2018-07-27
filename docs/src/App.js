66abec710af649f46489fd8b8afa03cd86b357cb
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-07-27 18:00:25 -0400

    inside src

--- docs/src/App.js
+++ docs/src/App.js
@@ -14,7 +14,7 @@ import {
 } from 'emotion-icons'
 
 import Fuse from 'fuse.js'
-import icons from '../manifest.json'
+import icons from './manifest.json'
 import { IconExplorer } from './components/IconExplorer'
 
 
