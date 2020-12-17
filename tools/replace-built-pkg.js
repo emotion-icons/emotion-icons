5f09913aedecfa60333a6f2bf51a50c5f819395c
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-12-17 21:13:41 +0000

    Format

--- tools/replace-built-pkg.js
+++ tools/replace-built-pkg.js
@@ -1,16 +1,15 @@
-
-const fg=require('fast-glob')
-const fs=require('fs-extra')
+const fg = require('fast-glob')
+const fs = require('fs-extra')
 
 async function run() {
-    const files=await fg(['**/package.built.json','!**/node_modules/**'])
+  const files = await fg(['**/package.built.json', '!**/node_modules/**'])
 
-    for(const file of files) {
-        await fs.move(file,file.replace('.built',''),{overwrite: true})
-    }
+  for (const file of files) {
+    await fs.move(file, file.replace('.built', ''), {overwrite: true})
+  }
 }
 
 run().catch((err) => {
-    console.log(err.stack)
-    process.exit(1)
+  console.log(err.stack)
+  process.exit(1)
 })
