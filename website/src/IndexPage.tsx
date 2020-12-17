45be42205560740ce953fb938758a25bba0808ef
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2020-12-17 14:01:32 -0600

    Fix Next Warnings

--- website/src/IndexPage.tsx
+++ website/src/IndexPage.tsx
@@ -10,6 +10,7 @@ const IconExplorer = dynamic(() => import('./components/IconExplorer'), {loading
 const IndexPage: React.SFC = () => (
   <Layout>
     <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <title>Emotion Icons - a React Components icon library</title>
     </Head>
     <div>
